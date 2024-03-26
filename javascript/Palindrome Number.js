618af1bc1ffb79c6df8ae45eff701182c6dc3c6d
Zafran Khan <zafrankhattak480@gmail.com>
2024-03-27 00:25:16 +0500

    Day 1

---
+++ javascript/Palindrome Number.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/palindrome-number/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    console.log("x: ",x)
+};
+
+isPalindrome(121)
